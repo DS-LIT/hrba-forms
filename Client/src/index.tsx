0b0b34f8c7cc897015d52a0281ee59ab4f7ee245
DESKTOP-E1MLM2P\Daniel <daniel@maptaskr.com>
2025-05-13 13:56:12 +0800

    Update form to create pdf and email

--- Client/src/index.tsx
+++ Client/src/index.tsx
@@ -1,29 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { ThemeContextProvider } from './context/themeContext';
-import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
-import { PublicClientApplication } from '@azure/msal-browser';
-import { MsalProvider } from '@azure/msal-react';
-import { msalConfig } from './authConfig';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ThemeContextProvider } from "./context/themeContext";
+import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter
+import { PublicClientApplication } from "@azure/msal-browser";
+import { MsalProvider } from "@azure/msal-react";
+import { msalConfig } from "./authConfig";
+import { SnackbarProvider } from "notistack";
+import Grow from "./components/grow";
 
 const msalInstance = new PublicClientApplication(msalConfig);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+	document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <MsalProvider instance={msalInstance}>
-      <BrowserRouter> {/* Wrap the app with BrowserRouter */}
-        <ThemeContextProvider>
-          <App />
-        </ThemeContextProvider>
-      </BrowserRouter>
-    </MsalProvider>
-  </React.StrictMode>,
+	<React.StrictMode>
+		<MsalProvider instance={msalInstance}>
+			<BrowserRouter>
+				<ThemeContextProvider>
+					<SnackbarProvider maxSnack={3} TransitionComponent={Grow}>
+						<App />
+					</SnackbarProvider>
+				</ThemeContextProvider>
+			</BrowserRouter>
+		</MsalProvider>
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
